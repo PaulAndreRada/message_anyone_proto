f1e3fa4b7f556c9d3025702f1bd36cae59e2144a
paulrada <info@paulrada.com>
2018-12-10 15:21:53 -0800

    ready to deploy on heroku

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,1 +1,5 @@
-export const URL = 'http://localhost:5000/'
+// Production
+export const URL = "https://warm-headland-43577.herokuapp.com/"
+
+// Development
+// export const URL = 'http://localhost:5000/'
